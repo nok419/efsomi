14a7ad333f1e875d90223460879bbfa558d63b26
nok419 <127610427+nok419@users.noreply.github.com>
2025-01-17 12:42:46 +0900

    soundlist error 3

--- src/data/environmentalSounds.tsx
+++ src/data/environmentalSounds.tsx
@@ -5,21 +5,24 @@ export const loadEnvironmentalSounds = async () => {
     {
       id: '1',
       name: 'Test Environmental Sound 1',
-      category: 'nature',
+      category: 'nature' as 'nature', // 修正
+      characteristics: ['calm', 'soothing'],
       src: '/audio/envsounds/test1.mp3',
       thumbnail: '/audio/envsounds/test1.jpg'
     },
     {
       id: '2',
       name: 'Test Environmental Sound 2',
-      category: 'urban',
+      category: 'urban' as 'urban', // 修正
+      characteristics: ['busy', 'dynamic'],
       src: '/audio/envsounds/test2.mp3',
       thumbnail: '/audio/envsounds/test2.jpg'
     },
     {
       id: '3',
       name: 'Test Environmental Sound 3',
-      category: 'nature',
+      category: 'nature' as 'nature', // 修正
+      characteristics: ['peaceful', 'ambient'],
       src: '/audio/envsounds/test3.mp3',
       thumbnail: '/audio/envsounds/test3.jpg'
     }
